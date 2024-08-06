fb51d7d665fa16e33b317103cca83f2b0f2c4896
Asdfghjkkllljd <staricinal49@gmail.com>
2024-08-06 03:26:05 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
-TOKEN = "" # Токен бота
-CHANNEL_ID = -100дад # ID канала со ставками
-ADMINS = [б] # ID админов
-BET_URL = "http://t.me/send?start=IV2r4tr4ZuYo" # Ссылка на счет
-CRYPTOPAY_TOKEN = "ка" # Тут CryptoPay токен
+TOKEN = "7150256945:AAGedLY5QdmyYnWRyXPOgAWUeXg8LjQnKBU" # Токен бота
+CHANNEL_ID = -1002243436081 # ID канала со ставками
+ADMINS = [6420802993] # ID админов
+BET_URL = "http://t.me/send?start=IVYuHgV3DGMT" # Ссылка на счет
+CRYPTOPAY_TOKEN = "249639:AASjYzwN8UY0fuBL37zy52D12SUNjNuDxwi" # Тут CryptoPay токен
 WIN_IMAGE = "win.jpg" # Название файла с картинкой выигрыша
 LOSE_IMAGE = "lose.jpg" # Название файла с картинкой проигрыша
-LOGS_ID = -100минус 100 не убирай # ID канала с оплатами
+LOGS_ID = -1002226990800 # ID канала с оплатами
 
-RULES_LINK = "https://t.me/c/2173472320/1029" # Ссылка на канал с правилами
-NEWS_LINK = "https://t.me/+rwY_Wce8gAo5Yzdh" # Ссылка на новостной канал
-OWNER_LINK = "https://t.me/velfrid" # Ссылка на владельца/поддержку
-PEREHOD_LINK = "@AQUA_Bets" # Ссылка на переходник
-BETS_LINK = "https://t.me/+rwY_Wce8gAo5Yzdh" # Ссылка на канал ставок
-CASINO_NAME = "AQUA.Bet" # Название казино
+RULES_LINK = "https://t.me/c/2154993852/4" # Ссылка на канал с правилами
+NEWS_LINK = "https://t.me/+YXO5t3AYIRgxZTdi # Ссылка на новостной канал
+OWNER_LINK = "https://t.me/SCAMUNIZDEL" # Ссылка на владельца/поддержку
+PEREHOD_LINK = "https://t.me/+YXO5t3AYIRgxZTdi" # Ссылка на переходник
+BETS_LINK = "https://t.me/extremecasinoo" # Ссылка на канал ставок
+CASINO_NAME = "EXTREME CASINO" # Название казино
